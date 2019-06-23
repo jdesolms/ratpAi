d822f599b2fe8724eac547e1a323e38dba522850
De Solms <jeansolm@amazon.fr>
2019-06-23 20:31:59 +0200

    first commit

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn index:app
 web: python index.py runserver 0.0.0.0:5000
--- index.py
+++ index.py
@@ -8,24 +8,6 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-# function for responses
-def results():
-    # build a request object
-    req = request.get_json(force=True)
-
-    # fetch action from json
-    action = req.get('queryResult').get('action')
-
-    # return a fulfillment response
-    return {'fulfillmentText': 'This is a response from webhook.'}
-
-# create a route for webhook
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():
-    # return response
-    return make_response(jsonify(results()))
-
-
 
     # run the app
 if __name__ == '__main__':
