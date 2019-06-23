1eb37bc0f8a6b8f054d398808635cc96fe1d43b3
De Solms <jeansolm@amazon.fr>
2019-06-23 20:57:47 +0200

    first commit

--- index.py
+++ index.py
@@ -10,14 +10,66 @@ def index():
 
 # function for responses
 def results():
-    # build a request object
     req = request.get_json(force=True)
-
-    # fetch action from json
     action = req.get('queryResult').get('action')
 
-    # return a fulfillment response
-    return {'fulfillmentText': 'La ligne est la {}.'.format(action)}
+    if action == "actions.intent.MAIN":
+        # your action statements here
+        # do whatever you want
+        # return response in dialogflow response format
+        # i am going to use Dialogflow JSON reponse format
+        # first build result json
+
+        result = {} # an empty dictionary
+
+        # fulfillment text is the default response that is returned to the dialogflow request
+        result["fulfillmentText"] = "your response message here"
+
+        # you can also make rich respones like basic card, simple responses, list, table card etc.
+        # you can refer this for rich response formats
+        # https://github.com/dialogflow/fulfillment-webhook-json
+
+        # you can also use custom payloads for different services like messenger or google assistant
+        # below is an example of google assistant payload
+        # the following paylod contains a simple response, a basic card and some suggestion chips.
+
+        # reply["payload"] = {
+            # "google": {
+            # "expectUserResponse": True,
+            # "richResponse": {
+                # "items": [
+                # {
+                    # "simpleResponse": {
+                        # "displayText": 'text to be displayed',
+                        # "textToSpeech": 'text that will be used for text to speech'
+                    # }
+                # },
+                # {
+                    # "basicCard": {
+                        # "title": "card title",
+                        # "subtitle": "card subtitle",
+                        # "imageDisplayOptions": "WHITE"
+                    # }
+                # }
+                # ],
+                # "suggestions": [
+                    # {
+                        # "title": "chip 1"
+                    # },
+                    # {
+                        # "title": "chip 2"
+                    # }
+                # ],
+            # }
+            # }
+        # }
+
+        # jsonify the result dictionary
+        # this will make the response mime type to application/json
+        result = jsonify(result)
+
+        # return the result json
+        return make_response(result)
 
 # create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
