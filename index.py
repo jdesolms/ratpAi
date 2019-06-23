007f180f18520ef4a462594b4be82fd6b793087d
De Solms <jeansolm@amazon.fr>
2019-06-23 20:47:56 +0200

    first commit

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ def results():
     req = request.get_json(force=True)
 
     # fetch action from json
-    action = req.get('queryResult').get('action')
+    action = req.get('parameters').get('number')
 
     # return a fulfillment response
     return {'fulfillmentText': 'La ligne est la {}.'.format(action)}
